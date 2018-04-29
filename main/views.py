ad561006edf0a9cd88b218c6b5c35f86fd441eca
katalyzator <web.coder96@gmail.com>
2018-04-29 16:34:57 +0600

    det finished

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 import logging
 
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.views.decorators.csrf import csrf_exempt
 
 logger = logging.getLogger(__name__)
@@ -19,14 +19,19 @@ def index_view(request):
         try:
             matrix = []
             row_count = request.POST.get('row_length')
-            for i in range(1, int(row_count) + 1):
+            # for i in range(0, int(row_count)):
+            #     for j in range(0, int(row_count)):
+            #         print i, j
+            for i in range(0, int(row_count)):
                 matrix.append(
-                    [int(request.POST.get('m_' + str(i) + '_' + str(j))) for j in range(1, int(row_count) + 1)], )
+                    [int(request.POST.get('m_' + str(i) + '_' + str(j))) for j in range(0, int(row_count))], )
 
-            for item in matrix:
-                print item
+            # for item in matrix:
+            #     print item
 
-            print np.linalg.det(matrix)
+            result = np.linalg.det(matrix)
+
+            return JsonResponse(dict(result=result))
 
         except Exception as exc:
 
@@ -42,8 +47,15 @@ def index_view(request):
     return render(request, template, context)
 
 
+@csrf_exempt
 def matrix_view(request):
     context = {}
     template = 'matrix.html'
 
     return render(request, template, context)
+
+# @csrf_exempt
+# def ajax_det_calc(request):
+#     response = JsonResponse(dict(result=result))
+#
+#     return response
